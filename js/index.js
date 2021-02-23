184c15bcd43b3a9d34891122ac1f19b1b4d1b585
Joelle Everaert <72984240+Joelle-Everaert@users.noreply.github.com>
2021-02-23 11:02:22 +0100

    cookies ok

--- js/index.js
+++ js/index.js
@@ -3,15 +3,19 @@ import React from 'react'
 
 
 import { NavBar } from './components/navbar.jsx'
+import { Cookies } from './components/cookies.jsx'
+
 
 function Application(){
     return(
     <div>
         <NavBar />
         <h1> Bonjour </h1>
+        <Cookies />
     </div>
     )
 }
+
 
 ReactDom.render(
     <Application />,
